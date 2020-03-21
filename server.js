0ab23fb346e83a7ffc890de6f6a190b868ecf1a3
veezyjay <viktorbasi545@gmail.com>
2020-03-21 12:22:37 +0100

    Connect app to mongo db

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect database
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactKeeper App" })
@@ -10,7 +14,6 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

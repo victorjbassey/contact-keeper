5f10c198b6f84c842d28687a5044c118785b69e4
veezyjay <viktorbasi545@gmail.com>
2020-03-21 13:14:04 +0100

    Add user model validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+
 const router = express.Router();
 
 // @route   POST  /api/users
 // @desc    Registers a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name").notEmpty(),
+    check("email", "Please use a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
+  }
+);
 
 module.exports = router;

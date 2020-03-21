0dfa41b17281d2de5bae777dc1c4377957901210
veezyjay <viktorbasi545@gmail.com>
2020-03-21 14:34:16 +0100

    Return token after successful login attempt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const router = express.Router();
 
@@ -34,7 +36,18 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      res.send("User saved");
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(payload, config.get("jwtSecret"), {
+        expiresIn: 360000
+      }, (err, token) => {
+        if (err) throw err;
+        res.status(200).json({ token });
+      })
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");

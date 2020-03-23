c7f6de2f2ec6845d611b541f02906b283d0b6d56
veezyjay <viktorbasi545@gmail.com>
2020-03-23 21:39:56 +0100

    Add private routes

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -91,6 +91,11 @@ const AuthState = props => {
   };
 
   // Logout
+  const logout = () => {
+    dispatch({
+      type: LOGOUT
+    });
+  };
 
   // Clear Errors
   const clearErrors = () => {
@@ -108,7 +113,8 @@ const AuthState = props => {
         register,
         clearErrors,
         loadUser,
-        login
+        login,
+        logout
       }}
     >
       {props.children}
